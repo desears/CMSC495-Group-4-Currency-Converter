be23d1bf407a04b0653c6910e150792e333b451c
auhroy <63337865+auhroy@users.noreply.github.com>
2021-06-25 11:25:58 -0400

    Update Currency.java

--- src/currencyapp/Currency.java
+++ src/currencyapp/Currency.java
@@ -1,5 +1,23 @@
-package currencyapp;
+/*
+* Author: Roy Auh
+* Course/Group: UMGC CMSC 495 Group 4
+* Description: This class specifies the currency object that holds the currency code (name) and its rate, based on USD being 1.
+*/
 
 public class Currency {
-    
+    private String name;
+    private Double rate;
+
+    public Currency(String n, Double r) {
+        this.name = n;
+        this.rate = r;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "name='" + name + '\'' +
+                ", rate=" + rate +
+                '}';
+    }
 }
