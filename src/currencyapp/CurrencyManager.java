525cba1a986f1517b6d8da802b310c8b9da66b34
Mike <Mike@Mikeys_Mac>
2021-06-26 17:18:59 -0400

    Redesigned CurrencyManager to use HashMap, which significantly cut down on the code to search

--- src/currencyapp/CurrencyManager.java
+++ src/currencyapp/CurrencyManager.java
@@ -1,6 +1,6 @@
 package currencyapp;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 /*
 * Author: Ronald DeSears
@@ -12,203 +12,30 @@ import java.util.ArrayList;
 */
 
 public class CurrencyManager {
-    String curr;
-    ArrayList <Currency> newC;
-    Currency c;
-    float m;
-    float newAmount;
+
+    HashMap<String, Currency> currencies;
     
-    // placeholder
+    // Constructor
+    public CurrencyManager() {
+        currencies = ExchangeRateWebService.retrieveRates();
+    }
+    
+    // This method converts an amount of currency1 to currency2 sourced from online
+    // rates
     public float convert(String currency1, String currency2, float amount) {
-        m = 0;
-        newAmount = amount;
-        ExchangeRateWebService rate = new ExchangeRateWebService();
-        rate.retrieveRates();
-        newC = rate.retrieveRates();
-        curr = currency2;
         
-    switch (currency1) { 
-        case "US Dollar":
-            searchRates();
-        break;
-        case "Euro":
-            convertUSD("EUR");
-            searchRates();
-            break;
-        case "British Pound":
-            convertUSD("GBP");
-            searchRates();
-            break;
-        case "Indian Rupee":
-            convertUSD("INR");
-            searchRates();
-            break;
-        case "Australian Dollar":
-            convertUSD("AUD");
-            searchRates();
-            break;
-        case "Canadian Dollar":
-            convertUSD("CAD");
-            searchRates();
-            break;
-        case "Singapore Dollar":
-            convertUSD("SGD");
-            searchRates();
-            break;
-        case "Swiss Franc":
-            convertUSD("CHF");
-            searchRates();
-            break;
-        case "Malaysian Ringgit":
-            convertUSD("MYR");
-            searchRates();
-            break;
-        case "Japanese Yen":
-            convertUSD("JPY");
-            searchRates();
-            break;
-        case "Chinese Yuan Renminbi":
-            convertUSD("CNY");
-            searchRates();
-            break;
-    }
-        return newAmount;
-    }
-        public void convertUSD(String u) {
-            for (int i = 0; i<newC.size(); i++) {
-                    if(newC.get(i).toString().contains(u)) {
-                    Currency c = newC.get(i);
-                    newAmount = newAmount/(float) c.getRate();
-                    //System.out.println(c.getRate());
-                    //System.out.println(c.toString());
-                    System.out.println(newAmount);
-                    }
-                }
+        // if both currencies are the same, no conversion required, return amount
+        if(currency1.equals(currency2))
+            return amount;
+        
+        // if the starting currency is not USD, we need to convert the amount to USD
+        // first in order to convert to currency2 since our rates are USD based
+        // This is done  by dividing amount/USDRate
+        if(!currency1.equals("US Dollar")) {
+            amount /= currencies.get(currency1).getRate();
         }
-        public void searchRates() {
-        if(curr == "US Dollar") {    
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("USD")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                System.out.println(m);
-                }
-            }
-        }    
-        if(curr == "Euro") {    
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("EUR")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                System.out.println(m);
-                }
-            }
-        }
-        else if(curr == "British Pound") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("GBP")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Indian Rupee") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("INR")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Australian Dollar") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("AUD")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Canadian Dollar") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("CAD")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Singapore Dollar") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("SGD")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Swiss Franc") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("CHF")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Malaysian Ringgit") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("MYR")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Japanese Yen") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("JPY")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-        else if(curr == "Chinese Yuan Renminbi") {
-            for (int i = 0; i<newC.size(); i++) {
-                if(newC.get(i).toString().contains("CNY")) {
-                Currency c = newC.get(i);
-                m = (float) c.getRate();
-                newAmount = m*newAmount;
-                System.out.println(c.getRate());
-                System.out.println(c.toString());
-                }
-            }
-        }
-    }
-    
-}
+        
+        return amount * (float) currencies.get(currency2).getRate();
+    } // end of method convert
+
+} // end of class CurrencyManager
